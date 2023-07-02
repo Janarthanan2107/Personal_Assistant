fcfbfa78ed858e5ba884d964b3c2a081845e293c
Janarthanan2107 <131163725+Janarthanan2107@users.noreply.github.com>
2023-07-02 14:08:04 +0530

    personal assistant Ui design has done!

---
+++ script/script.js
@@ -0,0 +1,15 @@
+// Using javascript file as strict file eliminating the silent errors into throwable errors
+'use strict';
+
+//selecting every elements from the html files in two types inputs(providing values) and outputs(displaying)
+//inputs
+
+//outputs
+
+//functions
+const init = ()=> {}
+
+//events
+
+//initial settings
+init();
